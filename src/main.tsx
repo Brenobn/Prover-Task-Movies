1d780e6a9fe100f6c25d001c7b75bb2accd79263
luanfiszer <luanbfiszer@gmail.com>
2025-10-23 14:42:54 -0300

    Criação arquivo CreateMovie

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,32 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./index.css"
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { App } from "./App";
-import { Home } from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { App } from "./App"
+import { CreateMovie } from "./pages/CreateMovie"
+import { Home } from "./pages/Home"
 
 const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-		children: [
-			{
-				path: "/",
-				element: <Home />,
-			},
-		],
-	},
-]);
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/criarfilme",
+        element: <CreateMovie />,
+      },
+    ],
+  },
+])
 
 // biome-ignore lint/style/noNonNullAssertion: React mandatory rule
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<RouterProvider router={router} />
-	</StrictMode>,
-);
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
---
+++ src/pages/CreateMovie.tsx
@@ -0,0 +1,7 @@
+
+
+export function CreateMovie() {
+  return(
+    
+  )
+}
