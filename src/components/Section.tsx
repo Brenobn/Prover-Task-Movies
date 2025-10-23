b1ed5cce5ab4067b7d6859c69b5cc51d6686f26f
Brenobn <brenobragabn@gmail.com>
2025-10-23 14:14:19 -0300

    Importando React e tipando children como Reac.ReactNode

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,8 +1,16 @@
 /** biome-ignore-all lint/a11y/useValidAnchor: Don't mind */
-export function Section() {
+import type React from "react"
+
+type SectionProps = {
+  children: React.ReactNode
+}
+
+export function Section({ children }: SectionProps) {
   return(
-    <a href="#" className="flex w-[77%] p-8 flex-col mr-5 items-start gap-4 rounded-2xl bg-[#FF859B10]">
+    <a href="#" className="flex w-[80%] p-8 flex-col mr-5 items-start gap-4 rounded-2xl bg-[#FF859B10]">
       <h2 className="text-white font-secondary font-bold text-2xl"> Interestellar</h2>
+
+      {children}
     </a>
   )
 }
