77c211f95e7c36f4d157916693c8d42161485421
Brenobn <brenobragabn@gmail.com>
2025-10-23 09:54:16 -0300

    Começando a contruir o componente Input

---
+++ src/components/Input.tsx
@@ -0,0 +1,7 @@
+export function Input() {
+  return (
+    <div className="w-full flex items-center bg-[#262529] text-[#948F99] rounded-[10px]">
+      <input className="w-full h-14 py-5 px-6 text-white bg-transparent border-0 placeholder:text-[#948F99]" placeholder="Pesquisar pelo título" />
+    </div>
+  )
+}
