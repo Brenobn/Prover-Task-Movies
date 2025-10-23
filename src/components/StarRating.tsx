71ab1b3209dd0056757c3062cf6c2044b6a08f66
Brenobn <brenobragabn@gmail.com>
2025-10-23 14:14:35 -0300

    Ajustando estilo

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -3,7 +3,7 @@ import { AiFillStar } from "react-icons/ai"
 export function StarRating() {
   return(
     <div className="flex gap-1.5">
-      <span className="text-[#FF859B]">
+      <span className="text-[#FF859B] inline-flex">
         <AiFillStar />
         <AiFillStar />
         <AiFillStar />
