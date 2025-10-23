2ae1f13f2bab1df4a07aeca8e6613995957e80b7
Brenobn <brenobragabn@gmail.com>
2025-10-23 14:12:20 -0300

    Importando dependencias do react-router-dom, criando uma constante chamada router, e configurando o RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { App } from "./App";
+import { Home } from "./pages/Home";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <Home />,
+			},
+		],
+	},
+]);
 
 // biome-ignore lint/style/noNonNullAssertion: React mandatory rule
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
+);
