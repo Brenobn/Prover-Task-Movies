9c1854acc97b143538edc64239083bc164666a31
Brenobn <brenobragabn@gmail.com>
2025-10-23 10:23:39 -0300

    Criando componente Tag.tsx

---
+++ src/components/Tag.tsx
@@ -0,0 +1,9 @@
+export function Tag() {
+  return(
+    <div className="flex px-1 py-4 items-start gap-2 rounded-lg bg-[#312E38]">
+      <p className="text-[#E5E5E5] text-center font-primary text-xs font-normal">
+        Ficção Científica
+      </p>
+    </div>
+  )
+}
