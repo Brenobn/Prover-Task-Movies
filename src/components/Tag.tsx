fd781a6c0d70efbfcaf7dc86dcb93dce18d7d805
Brenobn <brenobragabn@gmail.com>
2025-10-23 14:14:49 -0300

    Ajustando estilo

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,6 +1,6 @@
 export function Tag() {
   return(
-    <div className="flex px-1 py-4 items-start gap-2 rounded-lg bg-[#312E38]">
+    <div className="flex px-1.5 py-2 items-start gap-2 rounded-lg bg-[#312E38]">
       <p className="text-[#E5E5E5] text-center font-primary text-xs font-normal">
         Ficção Científica
       </p>
