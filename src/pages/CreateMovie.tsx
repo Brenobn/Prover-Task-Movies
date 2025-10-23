9f01329a482a65ae03ae53be3345b61f3630354c
luanfiszer <luanbfiszer@gmail.com>
2025-10-23 15:03:08 -0300

    Botão de voltar e titulo "Novo Filme"

--- src/pages/CreateMovie.tsx
+++ src/pages/CreateMovie.tsx
@@ -1,7 +1,20 @@
-
+import { FiArrowLeft } from "react-icons/fi"
+import { Link } from "react-router-dom"
 
 export function CreateMovie() {
-  return(
-    
+  return (
+    <div className="px-32 py-10">
+      <Link
+        className="inline-flex items-center gap-2 text-[#FF859B] no-underline"
+        to="/"
+      >
+        <FiArrowLeft />
+        Voltar
+      </Link>
+
+      <h1 className="mt-6 font-bold font-secondary text-4xl text-white">
+        Novo filme
+      </h1>
+    </div>
   )
 }
