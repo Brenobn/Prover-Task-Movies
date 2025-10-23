b65018ff747a7848d19edf384a90681bfa27e019
Brenobn <brenobragabn@gmail.com>
2025-10-23 15:45:21 -0300

    Importando Details e configurando path para ela

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { App } from "./App";
+import { Details } from "./pages/Details";
 import { Home } from "./pages/Home";
 
 const router = createBrowserRouter([
@@ -14,6 +15,10 @@ const router = createBrowserRouter([
 			{
 				path: "/",
 				element: <Home />,
+			},
+			{
+				path: "/detalhes",
+				element: <Details />,
 			},
 		],
 	},
