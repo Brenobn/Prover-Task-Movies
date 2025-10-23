8cee549d999229a9b229065e3ec0f17cc3175c37
Brenobn <brenobragabn@gmail.com>
2025-10-23 10:24:36 -0300

    Criando o componente Section.tsx

---
+++ src/components/Section.tsx
@@ -0,0 +1,8 @@
+/** biome-ignore-all lint/a11y/useValidAnchor: Don't mind */
+export function Section() {
+  return(
+    <a href="#" className="flex w-[77%] p-8 flex-col mr-5 items-start gap-4 rounded-2xl bg-[#FF859B10]">
+      <h2 className="text-white font-secondary font-bold text-2xl"> Interestellar</h2>
+    </a>
+  )
+}
